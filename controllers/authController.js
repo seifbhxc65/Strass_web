09b7e1fe7bdba5cb9005fde65167c890d5189368
seif belhaj <seifbhwc1@gmail.com>
2022-07-26 12:50:23 +0200

    starting up the auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+const Owner = require("../model/owner");
+const appError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.registerOwner = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, password, photo, email, address } = req.body;
+  const owner = await Owner.create({
+    firstName,
+    lastName,
+    password,
+    photo,
+    email,
+    address,
+  });
+  if(!owner) return next(new appError('an error happened during creation of the new owner',400));
+  res.status(201).json({
+    status:'sucess',
+    data:{
+        owner
+    }
+  })
+});
