8e0c4dd21721e19ae39f3bea7592e3a092be96f3
seif belhaj <seifbhwc1@gmail.com>
2022-07-27 12:02:21 +0200

    fix the err controller placement

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express=require('express');
 const app=express();
 const ownerRouter=require('./routes/ownerRouter');
+const appError = require('./utils/appError');
 const errCtr=require(`${__dirname}/controllers/errorController`)
 // app.get('/',(req,res)=>{
 //     res.send("we're ready !!");
 // })
+
 app.use(express.json({
     limit:'10kb'
   }));
-app.use(errCtr)
+
 app.use('/api/v1/owner',ownerRouter);
+app.use('*',(req,res,next)=>{
+    return next(new appError('this route is not defined',404))
+})
+app.use(errCtr)
 module.exports=app;
