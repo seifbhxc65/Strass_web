84ee4401a891930d24d69cbef97e73425c890277
seif belhaj <seifbhwc1@gmail.com>
2022-08-19 12:11:53 +0200

    adding menuId verif on mealRouter

--- routes/mealRouter.js
+++ routes/mealRouter.js
@@ -2,9 +2,12 @@ const express=require('express');
 const router=express.Router({mergeParams:true});
 const {getAllMeals,getOneMeal, updateMeal, deleteMeal, createMeal}=require('../controllers/mealController');
 
+const verifFieldExist = require('../middleware/verifFieldExist');
+const Menu = require('../model/Menu');
+
 router.route('/')
 .get(getAllMeals)
-.post(createMeal)
+.post(verifFieldExist(Menu,"menuId"),createMeal)
 router.route('/:id')
       .get(getOneMeal)
       .delete(deleteMeal)
