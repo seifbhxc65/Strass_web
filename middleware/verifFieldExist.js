8775d95a18b4b64ad5f65353ddd636537e2b6206
seif belhaj <seifbhwc1@gmail.com>
2022-08-23 06:12:23 +0200

    made inserting the rest/menu id in the body dynamic depends on the request

--- middleware/verifFieldExist.js
+++ middleware/verifFieldExist.js
@@ -2,8 +2,17 @@ const appError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 module.exports=(Model,field)=>catchAsync(async (req,res,next)=>{
-    const fieldValue=req.body[field];
-    if (!fieldValue) return next(new appError(`you need to provide the value of ${field}`,400));
+    let fieldValue=req.body[field];
+    if (!fieldValue) {
+        
+        if(req.params[field])
+            {
+                req.body[field]=req.params[field];
+                fieldValue=req.body[field];
+            }
+        else
+        return next(new appError(`you need to provide the value of ${field}`,400));
+    }
 
 const requiredDocument=await Model.findById(fieldValue);
 if(!requiredDocument) return next(new appError(`the provided ${field} does not correspond to any document`))
