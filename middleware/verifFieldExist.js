2ebd559b507212c501c1edeb3952ae42f19596a8
seif belhaj <seifbhwc1@gmail.com>
2022-09-05 17:14:22 +0200

    finished solving the bug

--- middleware/verifFieldExist.js
+++ middleware/verifFieldExist.js
@@ -1,8 +1,8 @@
-const { default: mongoose } = require("mongoose");
 const appError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 module.exports=(Model,field)=>catchAsync(async (req,res,next)=>{
+    
     let fieldValue=req.body[field];
     if (!fieldValue) {
         
@@ -15,15 +15,19 @@ module.exports=(Model,field)=>catchAsync(async (req,res,next)=>{
         return next(new appError(`you need to provide the value of ${field}`,400));
     }
 //
-const lastId= req.url.split('/')[1];
+const urlSplited=req.url.split('/');
+if(urlSplited.length>2) return next();
+const lastId= urlSplited[1];
 const requiredDocument= await Model.findOne({
     _id:fieldValue,
     ...req.params //menuId is included but it won't affect the query
 });
+console.log(requiredDocument);
+const type=field==="restaurantId"?"menu":"meals";
+let nestedElementIdsToVerify;
+if(requiredDocument) 
+nestedElementIdsToVerify=requiredDocument[type].map(el=>el._id.toString());
 
-
-const nestedElementIdsToVerify=requiredDocument?.menu.map(el=>el._id.toString());
-
-if(lastId&&nestedElementIdsToVerify.indexOf(lastId)==-1) return next(new appError(`the provided ${field} does not correspond to any document`))
+if(!requiredDocument || (lastId&&nestedElementIdsToVerify.indexOf(lastId)==-1)) return next(new appError(`the provided ${field} does not correspond to any document`))
 next();
 })
